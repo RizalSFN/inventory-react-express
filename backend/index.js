669d38fe92dc1d555d961a9aa2719fe66530ba29
RizalS <87477682+RizalSFN@users.noreply.github.com>
2024-06-28 21:30:50 +0700

    creating database

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port : ${port}`);
+});
