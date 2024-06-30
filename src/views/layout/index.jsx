3f168aaf9c3d96db079a74e134b7d4d29d02c07f
RizalS <87477682+RizalSFN@users.noreply.github.com>
2024-06-30 18:31:27 +0700

    setup layout

---
+++ src/views/layout/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SidebarMenu from "../../components/sidebarMenu";
+import NavbarMenu from "../../components/navbarMenu";
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex">
+      <SidebarMenu />
+      <div className="flex flex-col w-full">
+        <NavbarMenu />
+      </div>
+    </div>
+  );
+}
