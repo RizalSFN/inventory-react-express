642274b6dc4c7e63a3e0960639b65edf914ef7a5
RizalS <87477682+RizalSFN@users.noreply.github.com>
2024-06-30 23:46:09 +0700

    create products view

---
+++ src/views/products/products.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../layout";
+
+export default function Products() {
+  return (  
+    <Layout>
+      <h1 className="text-2xl mt-2 ml-2">Products</h1>
+    </Layout>
+  );
+}
